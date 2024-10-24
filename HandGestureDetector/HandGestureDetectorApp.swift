4db6141fd8fac3b1100b3f6b086b7a1ab6df74b2
Sebastian Kotarski <>
2024-10-24 21:24:17 +0200

    Start of the project

---
+++ HandGestureDetector/HandGestureDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandGestureDetectorApp.swift
+//  HandGestureDetector
+//
+//  Created by Course on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HandGestureDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
