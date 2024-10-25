76b546b96d208669ca5745cdd3f0f631acb3ea33
Sebastian Kotarski <>
2024-10-25 12:58:29 +0200

    Implement attachToMeshScene

--- HandGestureDetector/SessionHandler.swift
+++ HandGestureDetector/SessionHandler.swift
@@ -88,6 +88,49 @@ final class SessionHandler: NSObject, ObservableObject, ARSessionDelegate {
     private func performAttachMesh() {
         guard let arView else { return }
         
-        // todo: Attach mesh to scene
+        attachMeshToScene(in: arView)
+    }
+    
+    private func attachMeshToScene(in arView: ARSCNView) {
+        let boxGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.005)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        material.roughness.contents = 0.15
+        material.metalness.contents = 1.0
+        
+        boxGeometry.materials = [material]
+        
+        let boxNode = SCNNode(geometry: boxGeometry)
+        
+        let planeAnchor = ARAnchor(name: "horizontalPlane", transform: simd_float4x4(SCNMatrix4Identity))
+        
+        if let position = performRaycastForHorizontalPlane(in: arView) {
+            boxNode.position = position
+        }
+        
+        arView.session.add(anchor: planeAnchor)
+        
+        let anchorNode = SCNNode()
+        anchorNode.addChildNode(boxNode)
+        
+        arView.scene.rootNode.addChildNode(anchorNode)
+    }
+    
+    private func performRaycastForHorizontalPlane(in arView: ARSCNView) -> SCNVector3? {
+        let screenCenter = CGPoint(x: arView.bounds.midX, y: arView.bounds.midY)
+        
+        guard let raycastQuery = arView.raycastQuery(from: screenCenter, allowing: .estimatedPlane, alignment: .horizontal) else { return nil }
+        
+        let results = arView.session.raycast(raycastQuery)
+        
+        if let result = results.first {
+            let translation = result.worldTransform.columns.3
+            let position = SCNVector3(translation.x, translation.y, translation.z)
+            
+            return position
+        }
+        
+        return nil
     }
 }
